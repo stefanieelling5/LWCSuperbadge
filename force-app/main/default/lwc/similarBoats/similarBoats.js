c9ec3d5b6de5cdd8b951ee1d877f450484869bd0
Stefanie <stefanie.elling@capgemini.com>
2024-08-05 13:34:59 -0400

    Created components

---
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -0,0 +1,35 @@
+import { LightningElement } from 'lwc';
+
+// import getSimilarBoats
+export default class SimilarBoats extends LightningElement {
+    // Private
+    relatedBoats;
+    boatId;
+    error;
+    
+    // public
+    get recordId() {
+        // returns the boatId
+      }
+      set recordId(value) {
+          // sets the boatId value
+          // sets the boatId attribute
+      }
+    
+    // public
+    similarBy;
+    
+    // Wire custom Apex call, using the import named getSimilarBoats
+    // Populates the relatedBoats list
+    similarBoats({ error, data }) { }
+    get getTitle() {
+      return 'Similar boats by ' + this.similarBy;
+    }
+    get noBoats() {
+      return !(this.relatedBoats && this.relatedBoats.length > 0);
+    }
+    
+    // Navigate to record page
+    openBoatDetailPage(event) { }
+  }
+  
